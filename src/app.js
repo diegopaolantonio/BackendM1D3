a519aab271dc397ab22ad675e83620830052c391
diegopaolantonio <dpaolantonio.expertoing@gmail.com>
2023-03-08 00:20:47 -0300

    Commit 3 - Se eliminaron index.js y ProductManager.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
+const fs = require("fs");
+
 const express = require("express");
 
 const app = express();
 
-app.get("/saludo", (req, res) => {
-    res.send("Hola");
+class ProductManager {
+  constructor() {
+    this.path = "./files/fileproducts.json";
+  }
+
+  getProducts = async () => {
+    if (fs.existsSync(this.path)) {
+      const fileData = await fs.promises.readFile(this.path, "utf-8");
+      const result = JSON.parse(fileData);
+      return result;
+    } else {
+      return [];
+    }
+  };
+}
+
+const productManager = new ProductManager();
+
+app.get("/products", (req, res) => {
+  const products = productManager.getProducts();
+  res.send(products);
 });
 
 app.listen(8080, () => {
-    console.log("Servidor montado en direccion 8080");
+  console.log("Servidor montado en direccion 8080");
 });
