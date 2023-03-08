5e2bfbb0c0d160240d286d0db3abdcacb156d9d9
diegopaolantonio <dpaolantonio.expertoing@gmail.com>
2023-03-08 02:42:08 -0300

    Commit 4 - Se agregaron las funciones de imprimir completo o una cantidad definida por el usuario, y por un id especifico

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 
 const app = express();
 
+
 class ProductManager {
   constructor() {
     this.path = "./files/fileproducts.json";
@@ -18,14 +19,46 @@ class ProductManager {
       return [];
     }
   };
+
+  getProductById = async (productId) => {
+    const products = await this.getProducts();
+
+    const productIndex = products.findIndex(
+      (product) => product.id === productId
+    );
+
+    if (productIndex === -1) {
+      return "Not found";
+    }
+    return products[productIndex];
+  };
 }
+
 
 const productManager = new ProductManager();
 
-app.get("/products", (req, res) => {
-  const products = productManager.getProducts();
-  res.send(products);
+
+app.get("/products", async (req, res) => {
+  const limit = req.query.limit;
+  const products = await productManager.getProducts();
+  if (!limit) {
+    res.send(products);
+  } else {
+    let productsLimit = [];
+    for (i = 0; i < limit && i < products.length; i++) {
+      productsLimit.push(products[i]);
+    }
+    res.send(productsLimit);
+  }
 });
+
+
+app.get("/products/:pid", async (req, res) => {
+  const pid = parseInt(req.params.pid);
+  const product = await productManager.getProductById(pid);
+  res.send(product);
+});
+
 
 app.listen(8080, () => {
   console.log("Servidor montado en direccion 8080");
